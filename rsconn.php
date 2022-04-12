b8ea9f34a01db3ed2114e4a66db60fdd9518e1fe
AnnaL001 <43697008+AnnaL001@users.noreply.github.com>
2022-04-12 14:58:36 +0300

    Update rsconn.php

--- rsconn.php
+++ rsconn.php
@@ -1,5 +1,5 @@
 <?php
-  $conn=mysqli_connect("localhost","root","australiz1998JGH","ranking");
+  $conn=mysqli_connect("localhost","root","*********","ranking");
   if(mysqli_connect_errno()){
   	  echo "Failed to connect".mysqli_connect_error();
   }/*else{
